df7b44adb440d112b48f1db58bc94658ba676791
Esteban Alvarez Atehortua <estebanaa82@gmail.com>
2024-02-06 14:11:47 -0500

    adicion archivo prueba.py

---
+++ prueba.py
@@ -0,0 +1,8 @@
+import numpy as np
+import sympy as sym
+import matplotlib.pyplot as plt
+xi = np.array([0, 0.2, 0.3, 0.4])
+fi = np.array([1, 1.6, 1.7, 2.0])
+n = len(xi)
+x = sym.Symbol('x')
+polinomio = 0
